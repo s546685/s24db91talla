e8e56016b9ba27d9b1b6adc490f77704acd3f8d9
s546685 <164276517+s546685@users.noreply.github.com>
2024-04-05 20:42:58 -0500

    updated
    lab12 updated and pushed

--- controllers/watches.js
+++ controllers/watches.js
@@ -24,10 +24,20 @@ exports.watches_view_all_Page = async function (req, res) {
     }
 };
 // for a specific watches.
-exports.watches_detail = function (req, res) {
-    res.send('NOT IMPLEMENTED: watches detail: ' + req.params.id);
-};
+// for a specific Costume.
+exports.watches_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await watches.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+    };
+    
 // Handle watches create on POST.
+
 // Handle Costume create on POST.
 exports.watches_create_post = async function (req, res) {
     console.log(req.body)
@@ -56,6 +66,23 @@ exports.watches_delete = function (req, res) {
     res.send('NOT IMPLEMENTED: watches delete DELETE ' + req.params.id);
 };
 // Handle watches update form on PUT.
-exports.watches_update_put = function (req, res) {
-    res.send('NOT IMPLEMENTED: watches update PUT' + req.params.id);
+// Handle Costume update form on PUT.
+exports.watches_update_put = async function(req, res) {
+console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+try {
+let toUpdate = await watches.findById( req.params.id)
+// Do updates of properties
+if(req.body.watches_brand)
+toUpdate.watches_brand = req.body.watches_brand;
+if(req.body.watches_type) toUpdate.watches_type = req.body.watches_type;
+if(req.body.cost) toUpdate.cost = req.body.cost;
+let result = await toUpdate.save();
+console.log("Sucess " + result)
+res.send(result)
+} catch (err) {
+res.status(500)
+res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+}
 };
