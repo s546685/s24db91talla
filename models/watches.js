17330be537e2c53ba6183d0014af143f41d90a11
s546685 <164276517+s546685@users.noreply.github.com>
2024-04-17 13:43:55 -0500

    c2

--- models/watches.js
+++ models/watches.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose")
 const watchesSchema = mongoose.Schema({
 watches_brand: String,
-watches_type: String,
-cost: Number
+watches_type: {
+    type: String,
+    minlength:1,
+    maxlength: 14,
+},
+cost: {
+    type: Number,
+    min: 1,
+    max :2000,
+}
 })
 module.exports = mongoose.model("watches",
 watchesSchema)
--- routes/watches.js
+++ routes/watches.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var passport = require('passport');
 const watches_controlers= require('../controllers/watches');
 var router = express.Router();
 /* GET costumes */
@@ -6,12 +7,25 @@ router.get('/', watches_controlers.watches_view_all_Page );
 /* GET detail costume page */
 router.get('/detail', watches_controlers.watches_view_one_Page);
 /* GET create costume page */
-router.get('/create', watches_controlers.watches_create_Page);
+
 //module.exports = router;
 //router.put('/watches/:id', watches_controllers.watches_update_put);
 /* GET create update page */
-router.get('/update', watches_controlers.watches_update_Page);
+// A little function to check if we have an authorized user and continue on
+//or
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+res.redirect("/login");
+}
+router.get('/create',secured, watches_controlers.watches_create_Page);
+router.get('/update',secured, watches_controlers.watches_update_Page);
 router.get('/delete', watches_controlers.watches_delete_Page);
+router.post('/login', passport.authenticate('local'), function(req, res) {
+    res.redirect('/');
+    });
 module.exports = router;
 /* GET delete costume page */
 
